dbed70255fce4ca0f6817fa9fb1147935f4d2f34
user <Global@mail.ua>
2017-02-01 15:35:32 +0200

    save form authorization

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>form authorization</title>
+        <meta charset="UTF-8">
     </head>
     <body>
+		<form action="authorization.php" method="POST">
+            <input type="text" name="name" placeholder="Login..."/>
+            <input type="password" name="password" placeholder="Password..."/>
+            <input type="submit" value="authorization"/>
+        </form>
     </body>
 </html>
